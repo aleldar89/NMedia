f986ef14fbe8fbcce062fa44b6d13f108738984d
Эльдар Алиев <alel29643@gmail.com>
2022-05-26 00:01:49 +0300

    Доработка 2.1 События

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -14,22 +14,21 @@ class MainActivity : AppCompatActivity() {
             id = 1,
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36",
-            likedByMe = false
+            published = "21 мая в 18:36"
         )
         with(binding){
             author.text = post.author
             published.text = post.published
             content.text = post.content
             if (post.likedByMe) {
-                like?.setImageResource(R.drawable.ic_liked_24)
+                like.setImageResource(R.drawable.ic_liked_24)
             }
             likeCounter.text = counterString(post.liked)
             shareCounter.text = counterString(post.shared)
             viewed.text = shareCounter.text
-            avatar.setImageResource(R.drawable.ic_launcher_foreground)
+            avatar.setImageResource(R.drawable.ic_netology_48)
 
-            like?.setOnClickListener {
+            like.setOnClickListener {
                 post.likedByMe = !post.likedByMe
                 if (post.likedByMe) {
                     like.setImageResource(R.drawable.ic_liked_24)
@@ -42,7 +41,7 @@ class MainActivity : AppCompatActivity() {
                 likeCounter.text = counterString(post.liked)
             }
 
-            share?.setOnClickListener {
+            share.setOnClickListener {
                 post.shared++
                 shareCounter.text = counterString(post.shared)
                 viewed.text = shareCounter.text
